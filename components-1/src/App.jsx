074de70978cc5cc840181e48a4a066d2ba2fb4f6
DEVita42 <dvt.lorenzo@gmail.com>
2024-10-18 21:14:59 +0200

    Components-3

--- components-1/src/App.jsx
+++ components-1/src/App.jsx
@@ -1,9 +1,17 @@
 import Hello from "./Hello";
+import Message from "./Message";
 
 function App() {
   return (
     <div>
-      <Hello></Hello>
+        {/* Rendering the Hello component more than once risults in having more than one complete messagge 
+        since the Hello components contains the messagge components they get rendered in the correct way, 
+        rendering the messagge component alone in App risults instead in having more singular messages one after another  */}
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Message/>
+      <Message/>
     </div>
   );
 }
